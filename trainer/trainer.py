f3c14c7a3cda5c42ac4305884ba8e80c1581880e
Tejas Pote <tejaspote915@gmail.com>
2023-01-08 19:40:48 +0530

    Initial commit

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -23,7 +23,6 @@ class Trainer():
         keep_ratio_at_best_acc = 0.
         best_keep_ratio = 1.
         acc_at_best_keep_ratio = 0.
-        var_loss = 0.
 
         for epoch in range(1, args.max_epoch+1):
             logger.info("-"*30 + "Epoch start" + "-"*30)
@@ -37,7 +36,9 @@ class Trainer():
                     for layer in model.modules():
                         if isinstance(layer, MaskedMLP) or isinstance(layer, MaskedConv2d):
                             loss_val += args.alpha * torch.sum(torch.exp(-layer.threshold))
-                    loss_val += args.beta * var_loss
+                    
+                    if epoch != 1 and idx !=0:
+                        loss_val += args.beta * var_loss    
                 
                 optimizer.zero_grad() 
                 loss_val.backward()
